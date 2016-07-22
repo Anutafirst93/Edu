786c0f74c5a15015331bcbb72814181083951541
ANN <Anuta-first@mail.ru>
2016-07-22 15:19:32 +0300

    mes1

---
+++ edu-web/src/main/java/com/conf/WebAppInitializer.java
@@ -0,0 +1,26 @@
+package com.conf;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/**
+ *
+ * @author Ќюта
+ */
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class[] {WebConfig.class}; // We dont need any special servlet config yet.
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] {"/"};
+	}
+	
+}
